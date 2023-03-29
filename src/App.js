1ad818a6b92f21ebdc42e8dcb26c96751a45fa49
itsmechrissyj <123317393+itsmechrissyj@users.noreply.github.com>
2023-03-29 14:23:24 +0100

    adding footer and github link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
   <div className='App'>
-    <h2> Hello world </h2></div> );
+    <h2> Hello world </h2>
+    <footer>
+    This app was created by Christine Jones and is available on <a href="https://github.com/itsmechrissyj/react-final-project" target="_blank" rel="noreferrer">Github</a></footer> </div> );
   
 }
 
