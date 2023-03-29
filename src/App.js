28af10fbc709f0be8f700e1bd184cb558fc45b05
itsmechrissyj <123317393+itsmechrissyj@users.noreply.github.com>
2023-03-29 14:25:59 +0100

    adding blog link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   <div className='App'>
     <h2> Hello world </h2>
     <footer>
-    This app was created by Christine Jones and is available on <a href="https://github.com/itsmechrissyj/react-final-project" target="_blank" rel="noreferrer">Github</a></footer> </div> );
+    This project was created by <a href="https://www.itsmechrissyj.co.uk" target="_blank" rel="noreferrer"> Christine Jones</a> and is available on <a href="https://github.com/itsmechrissyj/react-final-project" target="_blank" rel="noreferrer">Github</a></footer> </div> );
   
 }
 
