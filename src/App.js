dc53eaeda117251cc4b06f26c40553673852fd5e
itsmechrissyj <123317393+itsmechrissyj@users.noreply.github.com>
2023-03-29 14:41:48 +0100

    installing bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+
 import './App.css';
 
 function App() {
   return (
   <div className='App'>
+    <div className="container">
     <h2> Hello world </h2>
+    <button className="btn btn-primary"> Hello </button>
     <footer>
-    This project was created by <a href="https://www.itsmechrissyj.co.uk" target="_blank" rel="noreferrer"> Christine Jones</a> and is available on <a href="https://github.com/itsmechrissyj/react-final-project" target="_blank" rel="noreferrer">Github</a></footer> </div> );
-  
+    This project was created by <a href="https://www.itsmechrissyj.co.uk" target="_blank" rel="noreferrer"> Christine Jones</a> and is available on <a href="https://github.com/itsmechrissyj/react-final-project" target="_blank" rel="noreferrer">Github</a></footer> </div> 
+    </div>);
 }
 
 export default App;
