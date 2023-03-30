500540d14c8880418c34638f6e6175b76e27c74b
itsmechrissyj <123317393+itsmechrissyj@users.noreply.github.com>
2023-03-30 15:03:45 +0100

    adding daily forecast template

--- src/FutureForecast.js
+++ src/FutureForecast.js
@@ -1,36 +1,39 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React , {useState} from "react";
 import "./FutureForecast.css";
 import axios from "axios";
+import FutureForecastDay from "./FututeForecastDay";
 
 
 export default function FutureForecast(props) {
+    let [loaded, setLoaded] = useState (false);
+    let [forecast, setForecast] = useState (null);
+
 function handleResponse (response){
-    console.log(response.data);
+setForecast(response.data.daily)
+setLoaded(true);
 }
-let longtitude = props.coordinates.lon;
-let latitude = props.coordinates.lat;
-    let apiKey = "96771e971243152d6b8948878c26adde";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
-    
-axios.get(apiUrl).then (handleResponse);
-return (
-    <div className="FutureForecast">
-        <div className="row">
-            <div className="col">
-                <div className="<FutureForecast-day">
-Thursday</div>
-<WeatherIcon code="01d" size={30} />
-<div className="FutureForecast-temperatures">
- <span className="FutureForecast-temperature-max">19°</span>
- <span className="FutureForecast-temperature-min">10°</span>
- </div>
 
 
-            </div>
 
+if (loaded) { 
+    return <div className="FutureForecast">
+    <div className="row">
+        <div className="col">
+            <FutureForecastDay data={forecast[0]} />
+        </div>
         </div>
 
-
-    </div>)
-}
+    
+    </div>  
+    
+ 
+} else {
+    let apiKey = "96771e971243152d6b8948878c26adde";
+    let longtitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
+        
+    axios.get(apiUrl).then (handleResponse);
+    
+    return null;
+}}
