126270f0974fa7ee96d47e8a8eee82707ccb4a4b
itsmechrissyj <123317393+itsmechrissyj@users.noreply.github.com>
2023-03-30 13:12:09 +0100

    realigning components

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,24 +8,30 @@ return (
 <div className="WeatherInfo"> 
 <h1> {props.data.city} </h1>  
 <ul> 
-<li> <FormattedDate date={props.data.date} /> </li>
+<li> 
+    <FormattedDate date={props.data.date} /> 
+</li>
 <li className="text-capitalize"> {props.data.description} </li>
 </ul>
-<div className="row">
+<div className="row mt-3">
 <div className="col-6">
+    <div className="d-flex">
+        <div>
     <WeatherIcon code={props.data.icon} />
-    
+    </div>
+    <div>
 <WeatherTemperature celsius={props.data.temperature}/>
-
-    
+</div>
+</div>
+</div>
     <div className="col-6">
         <ul>
       <li>  Feels like: {Math.round(props.data.feelslike)} °C</li>
       <li>   Humidity: {props.data.humidity}%</li>
       <li>  Wind: {Math.round(props.data.wind)} km/h</li>
         </ul>
-    </div></div>
-
-</div>
+    </div>
+    </div>
+    
 
 </div> );}
